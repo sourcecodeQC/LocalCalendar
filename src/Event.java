c465323fc7760fd7e8f823f095b452e8a468b914
scQC <alek.de.mehlem@gmail.com>
2025-01-12 20:58:53 +0100

    event class updated to accept start and endtime

--- src/Event.java
+++ src/Event.java
@@ -1,10 +1,14 @@
 public class Event {
     private String title;
-    private String date; // For simplicity, using String for date
+    private String date; // yyy-mm-dd
+    private int startTime; // in minutes from midnight
+    private int endTime; //ditto
 
-    public Event(String title, String date) {
+    public Event(String title, String date, int startTime, int endTime) {
         this.title = title;
         this.date = date;
+        this.startTime = startTime;
+        this.endTime = endTime;
     }
 
     public String getTitle() {
@@ -14,12 +18,21 @@ public class Event {
     public String getDate() {
         return date;
     }
+    public int getStartTime() {
+        return startTime;
+    }
+
+    public int getEndTime() {
+        return endTime;
+    }
 
     @Override
     public String toString() {
-        return "Event{" +
+        return "Event{" + //external
                 "title='" + title + '\'' +
                 ", date='" + date + '\'' +
+                ", startTime=" + startTime +
+                ", endTime=" + endTime +
                 '}';
     }
 }
