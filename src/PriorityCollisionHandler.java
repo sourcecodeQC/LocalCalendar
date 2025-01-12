de615c5a4d747bfef5ea24006829f819ece86ac6
scQC <alek.de.mehlem@gmail.com>
2025-01-12 23:19:31 +0100

    framework of piority handling

--- src/PriorityCollisionHandler.java
+++ src/PriorityCollisionHandler.java
@@ -2,20 +2,33 @@ import java.util.List;
 import java.util.Scanner;
 
 public class PriorityCollisionHandler {
-    public static boolean checkCollision(List<Event> events, String newDate, int newStartTime, int newEndTime, int priority){
-        for (int i = 0; i < events.size(); i++) {
-            Event event = events.get(i);
-            String eventDate = event.getDate();
-
-
-            if(eventDate != null && eventDate.equals(newDate) &&  //.equal why? https://www.w3schools.com/java/ref_string_equals.asp#:~:text=The%20equals()%20method%20compares,to%20compare%20two%20strings%20lexicographically.
-                    newStartTime < event.getEndTime() &&
-                    newEndTime > event.getStartTime()) {
-                return true; // Collision detected
+    public static boolean checkCollision(List<Event> events, Event newEvent){
+        for(Event existingEvent : events){
+            if(existingEvent.getDate().equals(newEvent.getDate()) && isOverlapping(existingEvent, newEvent)) {//.equal why? https://www.w3schools.com/java/ref_string_equals.asp#:~:text=The%20equals()%20method%20compares,to%20compare%20two%20strings%20lexicographically.
+                // Collision = true
+                return
             }
-
-
         }
-        return false; // No collision
     }
+
+    private static boolean isOverlapping(Event existingEvent, Event newEvent){
+        return newEvent.getStartTime() < existingEvent.getEndTime() &&
+                newEvent.getEndTime() > existingEvent.getStartTime();
+
+    }
+
+    private static boolean handleCollision(Event existingEvent, Event newEvent){
+        if(existingEvent.getPriority() < newEvent.getPriority()){
+
+
+        } else if (existingEvent.getPriority() > newEvent.getPriority()) {
+            
+        } else {
+            //both events have same priority
+        }
+
+    }
+
+
 }
+
