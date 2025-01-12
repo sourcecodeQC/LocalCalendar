3e9eedee8a80c96aa90e34905966d5663c2efc92
scQC <alek.de.mehlem@gmail.com>
2025-01-13 00:13:00 +0100

    framework of IO

---
+++ src/FlieManagerIO.java
@@ -0,0 +1,6 @@
+import java.nio.*; // java.io.Serializable - suggested by ai; learned from external source https://docs.oracle.com/javase/8/docs/api/java/io/Serializable.html and https://www.geeksforgeeks.org/serializable-interface-in-java/
+import java.io.*;
+import java.util.*;
+
+public class FlieManagerIO {
+}
